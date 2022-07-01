00ab707e5b5bec7ca986e3a7eb9af952247a6fe0
pedroCurioni <pedrocurioni@gmail.com>
2022-07-01 17:12:45 -0300

    recuperar senha existe

---
+++ src/pages/RecuperarSenha/index.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import { ActivityIndicator, ActivityIndicatorBase, StyleSheet, View } from 'react-native';
+import { Button, Input, Text } from 'react-native-elements';
+
+const RecuperarSenha = () => {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [isLoading, setLoading] = useState(false);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.tituloContainer}>
+        <Text style={styles.titulo}>{'Recupere sua senha'}</Tex>
+      </View>
+      <Input
+        placeholder="E-mail"
+        onChangeText={setEmail}
+        inputContainerStyle={styles.inputContainer}
+        value={email}
+        placeholderTextColor={'#a49595'}
+      />
+      <Input
+        secureTextEntry={true}
+        placeholder="Senha"
+        onChangeText={setSenha}
+        inputContainerStyle={styles.inputContainer}
+        value={senha}
+        placeholderTextColor={'#a295a4'}
+      />
+      {isLoading === false ? (
+        <Button
+          buttonStyle={styles.button}
+          title="Login"
+          titleStyle={styles.buttonTitle}
+          // onPress={() => {
+          //   handleLogin(email, senha);
+          //   setLoading(true);
+          // }}
+        />
+      ) : (
+        <ActivityIndicator size="large" color="#fff" />
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 16,
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+  tituloContainer: {
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  titulo: {
+    color: '#000',
+    textAlign: 'center',
+    fontSize: 40,
+    fontWeight: 'bold',
+    marginBottom: 70,
+  },
+  inputContainer: {
+    boderColor: '#cfcfcf',
+    borderRadius: 15,
+    borderStyle: 'solid',
+    borderWidth: 3,
+    backgroundColor: '#fff',
+    padding: 5,
+    borderRadius: 10,
+    margin: -8,
+  },
+  button: {
+    backgroundColor: '#EE4249',
+    borderRadius: 10,
+    padding: 18,
+    marginTop: 70,
+  },
+  buttonTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
