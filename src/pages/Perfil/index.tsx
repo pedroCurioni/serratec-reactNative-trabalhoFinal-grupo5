0557a4f01e82274c1fb8ad385094f689994ccc16
pedroCurioni <pedrocurioni@gmail.com>
2022-07-01 22:02:51 -0300

    perfil sem imagem

--- src/pages/Perfil/index.tsx
+++ src/pages/Perfil/index.tsx
@@ -1,28 +1,144 @@
-import React, { useEffect, useState, useContext } from 'react';
-import { View, StyleSheet, ScrollView, FlatList, ActivityIndicator, TouchableOpacity, Keyboard } from 'react-native';
-import { Text, Input, Icon, Image, Card } from 'react-native-elements';
-import { AxiosInstance } from '../../api/AxiosInstance';
-import { AuthContext } from '../../context/AuthContext'
+import React, {useState} from 'react';
+import {
+  ActivityIndicator,
+  ActivityIndicatorBase,
+  StyleSheet,
+  View,
+} from 'react-native';
+import {Button, Input, Text} from 'react-native-elements';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 const Perfil = ({navigation}) => {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [imagem, setImagem] = useState('');
+  const [isLoading, setLoading] = useState(false);
 
-    return (   
-      <View style={styles.container}>
-        <Text style={styles.text}>Perfil</Text> 
-      </View>         
-    );
+  const handleAlterarDados = () => {
+    console.log('nome: ', nome, 'email: ', email, 'imagem: ', imagem);
+  };
+
+  return (
+    <View style={styles.body}>
+      <View style={styles.boxTitulo}>
+        <TouchableOpacity
+          style={styles.touchableContainer}
+          onPress={() => navigation.navigate('Login')}>
+          <Icon
+            name="left"
+            size={25}
+            color="#EE4249"
+            style={styles.botaoVoltar}
+          />
+        </TouchableOpacity>
+        <Text style={styles.titulo}>Perfil</Text>
+        <TouchableOpacity
+          style={styles.touchableContainer}
+          onPress={() => navigation.navigate('Login')}>
+          <Icon
+            name="logout"
+            size={25}
+            color="#EE4249"
+            style={styles.botaoVoltar}
+          />
+        </TouchableOpacity>
+      </View>
+      <Input
+        placeholder="Nome Completo"
+        onChangeText={setNome}
+        inputContainerStyle={styles.inputContainer}
+        value={nome}
+        placeholderTextColor={'#a49595'}
+      />
+      <Input
+        placeholder="E-mail"
+        onChangeText={setEmail}
+        inputContainerStyle={styles.inputContainer}
+        value={email}
+        placeholderTextColor={'#a295a4'}
+      />
+      <Input
+        placeholder="Endereço imagem"
+        onChangeText={setImagem}
+        inputContainerStyle={styles.inputContainer}
+        value={imagem}
+        placeholderTextColor={'#a295a4'}
+      />
+      <View style={styles.containerButtons}>
+        {isLoading === false ? (
+          <Button
+            buttonStyle={styles.button}
+            title="Trocar senha"
+            titleStyle={styles.buttonTitle}
+            onPress={() => {
+              handleAlterarDados();
+              setLoading(true);
+            }}
+          />
+        ) : (
+          <ActivityIndicator size="large" color="#EE4249" />
+        )}
+      </View>
+    </View>
+  );
 };
 
-export const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff'
-    },
-    text: {
-        fontSize: 20,
-        color: '#000',
-        marginLeft: 15
-    },
+const styles = StyleSheet.create({
+  body: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+  },
+  boxTitulo: {
+    flexDirection: 'row',
+    width: '100%',
+
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 70,
+    marginTop: 20,
+    marginHorizontal: 50,
+  },
+  touchableContainer: {
+    width: 26,
+    height: 26,
+  },
+  botaoVoltar: {},
+  titulo: {
+    width: '80%',
+    color: '#000',
+    textAlign: 'center',
+    fontSize: 26,
+    fontWeight: 'bold',
+  },
+  inputContainer: {
+    padding: 2,
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: '#cfcfcf',
+    borderRadius: 10,
+    margin: -8,
+    marginHorizontal: 10,
+  },
+  containerButtons: {
+    alignItems: 'center',
+  },
+  button: {
+    marginTop: 15,
+    width: 300,
+    backgroundColor: '#EE4249',
+    borderRadius: 10,
+    padding: 10,
+    borderWidth: 3,
+    borderStyle: 'solid',
+    borderColor: '#EE4249',
+  },
+  buttonTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
 });
 
 export default Perfil;
