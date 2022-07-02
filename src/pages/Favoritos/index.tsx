58f3e9e92532d4b9e621fec7c3db7ba3fa2c1e07
Kauã Cassiano <87822196+KhawanC@users.noreply.github.com>
2022-07-02 00:02:45 -0300

    pagina de favorito por enquanto com dado mokado

--- src/pages/Favoritos/index.tsx
+++ src/pages/Favoritos/index.tsx
@@ -1,14 +1,60 @@
-import React, { useEffect, useState, useContext } from 'react';
-import { View, StyleSheet, ScrollView, FlatList, ActivityIndicator, TouchableOpacity, Keyboard } from 'react-native';
-import { Text, Input, Icon, Image, Card } from 'react-native-elements';
-import { AxiosInstance } from '../../api/AxiosInstance';
-import { AuthContext } from '../../context/AuthContext'
+import React from 'react';
+import { View, StyleSheet, FlatList } from 'react-native';
+import { Text } from 'react-native-elements';
+import IconMat from 'react-native-vector-icons/MaterialIcons'
+import CardsFavoritos from '../../components/cardsFavoritos';
 
 const Favoritos = ({ navigation }) => {
 
+  const numColums = 3;
+
+  const produtos = [
+    {
+      id: '0',
+      preco: 11.99,
+      nome: 'Sorvete',
+      descricao: 'Unidade 900ml',
+      imagem: 'https://i.imgur.com/mJL5hao.png'
+    },
+    {
+      id: '1',
+      preco: 25.99,
+      nome: 'Energético Monster',
+      descricao: 'Unidade 900ml',
+      imagem: 'https://i.imgur.com/SwWp69v.png'
+    },
+    {
+      id: '2',
+      preco: 20.99,
+      nome: 'Skol',
+      descricao: 'Unidade 900ml',
+      imagem: 'https://i.imgur.com/Z83tdHE.png'
+    },
+    {
+      id: '3',
+      preco: 20.99,
+      nome: 'Pizza Mussarela',
+      descricao: '1 unidade',
+      imagem: 'https://i.imgur.com/LEDrUlQ.png'
+    }
+  ]
+
+  console.log(produtos.length)
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Favoritos</Text>
+      <View style={styles.boxTitulo}>
+        <Text style={styles.tituloFavorito}>Favoritos</Text>
+        <IconMat name='logout' size={25} color='#EE4249' style={styles.botaoLogout}/>
+      </View>
+      <FlatList
+        data={produtos}
+        contentContainerStyle={{paddingTop: 30, alignItems: 'center'}}
+        keyExtractor={item => item.id}
+        numColumns= {numColums}
+        renderItem={({item}) => <CardsFavoritos  imagem={item.imagem} preco={item.preco} descricao={item.descricao} nome={item.nome}/>}
+      />
+      
     </View>
   );
 };
@@ -18,11 +64,23 @@ export const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff'
   },
-  text: {
-    fontSize: 20,
-    color: '#000',
-    marginLeft: 15
+  boxTitulo: {
+    flexDirection: 'row',
+    marginTop: 40,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginHorizontal: 15
   },
+  tituloFavorito: {
+    fontSize: 26,
+    fontWeight: '700',
+  },
+  botaoLogout: {
+    paddingLeft: 160
+  },
+  botaoVoltar: {
+    paddingLeft: 25
+  }
 });
 
 export default Favoritos;
