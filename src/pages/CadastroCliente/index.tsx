415e79942deddcea7ff13fadf23decc0b3162667
Kauã Cassiano <87822196+KhawanC@users.noreply.github.com>
2022-07-03 17:35:33 -0300

    Pop up e alguns exceptions no cadastro

--- src/pages/CadastroCliente/index.tsx
+++ src/pages/CadastroCliente/index.tsx
@@ -1,58 +1,149 @@
 /* eslint-disable prettier/prettier */
-import React from 'react';
-import { View, StyleSheet, ScrollView, FlatList, ActivityIndicator, TouchableOpacity, Keyboard } from 'react-native';
-import { Text, Input, Image, Card, Button } from 'react-native-elements';
+import React, { useState } from 'react';
+import { View, StyleSheet, TouchableOpacity, Keyboard, TouchableWithoutFeedback, Modal } from 'react-native';
+import { Text, Input, Button, normalize } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/AntDesign'
 
 const CadastroCliente = ({navigation}:any) => {
 
-  return (   
-        <View style={styles.container}>
-          <View style={styles.boxTitulo}>
-            <TouchableOpacity onPress={() => navigation.navigate('Login')}>
-            <Icon name='left' size={25} color='#EE4249' style={styles.botaoVoltar}/>
-            </TouchableOpacity>
-            <Text style={styles.titulo}>Complete seu cadastro para fazer o pedido</Text>
-          </View>
-          <View style={styles.boxInputs}>
-            <Input      
-              placeholder="Nome Completo"
-              inputContainerStyle={styles.inputContainer}
-            />
-            <Input      
-              placeholder="E-mail"
-              inputContainerStyle={styles.inputContainer}
-            />
-            <Input      
-              placeholder="Endereço Imagem"
-              inputContainerStyle={styles.inputContainer}
-            />
-            <Input      
-              placeholder="Digite sua senha"
-              inputContainerStyle={styles.inputContainer}
-            />
-            <Input      
-              placeholder="Confirmar senha"
-              inputContainerStyle={styles.inputContainer}
-            />
-          </View>
-          <View style={styles.boxLastBotao}>
+  const [nome, setNome] = useState('')
+  const [email, setEmail] = useState('')
+  const [imagem, setImagem] = useState('')
+  const [senha, setSenha] = useState('')
+  const [confirmSenha, setConfirmSenha] = useState('')
+  const [isLoading, setLoading] = useState(false)
+  const [isPopupError, setPopupError] = useState(false)
+  const [errorMessage, setErrorMessage] = useState()
+
+  function nomeNumRegex(str:string) {
+    return /[0-9]/.test(str)
+  }
+
+  function load() {
+    setTimeout(function() {
+      setLoading(false)
+      setPopupError(true)
+      console.log(nome + '|' +  email + '|' + imagem + '|' + senha + '|' + confirmSenha)
+      if(nome === ''  || email === '' || imagem === '' || senha === '' || confirmSenha === '') {
+        setErrorMessage(e => 'Um de seus campos está vazio!')
+      } else if(nomeNumRegex(nome)) {
+        setErrorMessage(e => 'Seu nome contém caracteres inválidos!')
+      } else if (senha.length < 5) {
+        setErrorMessage(e => 'Sua senha é muito fraca!')
+      } else if(senha != confirmSenha) {
+        setErrorMessage(e => 'Suas senhas não coincidem!')
+      }
+    }, 1000)
+  }
+
+  return (
+  <HideKeyboard>
+    <View style={styles.container}>
+      <View style={styles.boxTitulo}>
+        <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+        <Icon name='left' size={25} color='#EE4249' style={styles.botaoVoltar}/>
+        </TouchableOpacity>
+        <Text style={styles.titulo}>Complete seu cadastro para fazer o pedido</Text>
+      </View>
+      <View style={styles.boxInputs}>
+        <Input      
+          placeholder="Nome Completo"
+          inputContainerStyle={styles.inputContainer}
+          onChangeText={setNome}
+        />
+        <Input      
+          placeholder="E-mail"
+          inputContainerStyle={styles.inputContainer}
+          onChangeText={setEmail}
+        />
+        <Input      
+          placeholder="Endereço Imagem"
+          inputContainerStyle={styles.inputContainer}
+          onChangeText={setImagem}
+        />
+        <Input
+          secureTextEntry={true}      
+          placeholder="Digite sua senha"
+          inputContainerStyle={styles.inputContainer}
+          onChangeText={setSenha}
+        />
+        <Input     
+           secureTextEntry={true}
+          placeholder="Confirmar senha"
+          inputContainerStyle={styles.inputContainer}
+          onChangeText={setConfirmSenha}
+        />
+      </View>
+      <View style={styles.boxLastBotao}>
+        {isLoading ? <Button
+          loading
+          buttonStyle={styles.botaoConfirmar}
+          titleStyle={styles.textoBotaoConfirmar}
+          title="Cadastrar"
+        /> : <Button
+        buttonStyle={styles.botaoConfirmar}
+        titleStyle={styles.textoBotaoConfirmar}
+        title="Cadastrar"
+        onPress={() => {setLoading(true); load()}}
+      />}
+      </View>
+      <Modal
+        animationType={'slide'}
+        transparent={true}
+        visible={isPopupError}
+        onRequestClose={() => setPopupError(false)}>
+        <View style={styles.boxModal}>
+          <View style={styles.modal}>
+            <Text style={styles.tituloPopup}>Erro ao se registrar</Text>
+            <Text style={styles.subTitlePopup}>{errorMessage}</Text>
             <Button
-              buttonStyle={styles.botaoConfirmar}
-              titleStyle={styles.textoBotaoConfirmar}
-              title="Cadastrar"
+              title='Voltar'
+              containerStyle={styles.boxBotaoPopup}
+              buttonStyle={styles.buttaoPopup}
+              onPress={() => {setPopupError(false); setErrorMessage(e => '')}}
             />
           </View>
-          
-        </View>        
+        </View>
+        
+      </Modal>
+    </View>
+  </HideKeyboard>
+               
   );
 };
+
+const HideKeyboard = ({ children }) => (
+  <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+    {children}
+  </TouchableWithoutFeedback>
+)
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
+    justifyContent: 'center',
     backgroundColor: '#fff'
+  },
+  boxModal: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.3)'
+  },
+  modal: {
+    marginTop: 290,
+    backgroundColor: "white",
+    borderRadius: 20,
+    margin: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
   },
   boxTitulo: {
     flexDirection: 'row',
@@ -60,7 +151,7 @@ export const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     marginTop: 60,
-    marginBottom: 135,
+    marginBottom: 70,
     marginHorizontal: 66
   },
   botaoVoltar: {
@@ -97,7 +188,25 @@ export const styles = StyleSheet.create({
   textoBotaoConfirmar: {
     fontSize: 20,
     fontWeight: '700'
-  }
+  },
+  tituloPopup: {
+    fontSize: 18,
+    fontWeight: '700',
+  },
+  subTitlePopup: {
+    marginTop: 9,
+    fontSize: 15,
+    fontWeight: '500',
+  },
+  boxBotaoPopup: {
+    marginTop: 30
+  },
+  buttaoPopup: {
+    backgroundColor: '#EE4249',
+    borderRadius: 10,
+    padding: 2,
+    width: 90,
+  },
 });
 
 export default CadastroCliente;
