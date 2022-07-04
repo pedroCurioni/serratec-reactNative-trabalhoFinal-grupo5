6789dcf7b80a2703c5874e9af4fdd5f2be8b531e
pedroCurioni <pedrocurioni@gmail.com>
2022-07-04 16:25:17 -0300

    perfil mostra informações do usuario

--- src/pages/Perfil/index.tsx
+++ src/pages/Perfil/index.tsx
@@ -1,70 +1,58 @@
-import React, { useEffect, useState } from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {
   ActivityIndicator,
   StyleSheet,
   View,
   Image,
-  TouchableOpacity
+  TouchableOpacity,
 } from 'react-native';
-import { Button, Input, Text } from 'react-native-elements';
+import {Button, Input, Text} from 'react-native-elements';
 import Icon from 'react-native-vector-icons/AntDesign';
+import {AuthContext} from '../../context/AuthContext';
 
-const Perfil = ({ navigation }) => {
-  const [nome, setNome] = useState('');
-  const [email, setEmail] = useState('');
-  const [imagem, setImagem] = useState('');
+const Perfil = ({navigation}) => {
+  const {user} = useContext(AuthContext);
   const [isLoading, setLoading] = useState(false);
-
-  const handleAlterarDados = () => {
-    console.log('nome: ', nome, 'email: ', email, 'imagem: ', imagem);
-  };
 
   return (
     <View style={styles.container}>
       <View style={styles.boxTitulo}>
         <TouchableOpacity onPress={() => navigation.navigate('HomeTabScreen')}>
-          <Icon name='left' size={25} color='#EE4249' style={styles.botaoVoltar} />
+          <Icon
+            name="left"
+            size={25}
+            color="#EE4249"
+            style={styles.botaoVoltar}
+          />
         </TouchableOpacity>
         <Text style={styles.titulo}>Perfil</Text>
-        <TouchableOpacity style={styles.logoff} onPress={() => navigation.navigate('Login')}>
-          <Image source={require('../../assets/logout.png')} style={styles.imageLogoff} />
+        <TouchableOpacity
+          style={styles.logoff}
+          onPress={() => navigation.navigate('Login')}>
+          <Image
+            source={require('../../assets/logout.png')}
+            style={styles.imageLogoff}
+          />
         </TouchableOpacity>
       </View>
       <Image
         source={require('../../assets/imagem_perfil.png')}
         style={styles.image}
       />
-      <Input
-        placeholder="Nome Completo"
-        onChangeText={setNome}
-        inputContainerStyle={styles.inputContainer}
-        value={nome}
-        placeholderTextColor={'#a49595'}
-      />
-      <Input
-        placeholder="E-mail"
-        onChangeText={setEmail}
-        inputContainerStyle={styles.inputContainer}
-        value={email}
-        placeholderTextColor={'#a295a4'}
-      />
-      <Input
-        placeholder="Endereço imagem"
-        onChangeText={setImagem}
-        inputContainerStyle={styles.inputContainer}
-        value={imagem}
-        placeholderTextColor={'#a295a4'}
-      />
+      <View style={styles.textContainer}>
+        <Text style={styles.textStyle}>Nome: {user.name}</Text>
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.textStyle}>Email: {user.email}</Text>
+      </View>
+
       <View style={styles.containerButtons}>
         {isLoading === false ? (
           <Button
             buttonStyle={styles.button}
-            title="Alterar Dados"
+            title="Alterar senha"
             titleStyle={styles.buttonTitle}
-            onPress={() => {
-              handleAlterarDados();
-              setLoading(true);
-            }}
+            onPress={() => navigation.navigate('RecuperarSenha')}
           />
         ) : (
           <ActivityIndicator size="large" color="#EE4249" />
@@ -105,17 +93,20 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontSize: 24,
     fontWeight: 'bold',
-    paddingLeft: 10
+    paddingLeft: 10,
   },
-  inputContainer: {
-    padding: 2,
+  textContainer: {
+    padding: 6,
     backgroundColor: '#fff',
     borderWidth: 1,
     borderStyle: 'solid',
     borderColor: '#cfcfcf',
     borderRadius: 10,
-    margin: -8,
-    marginHorizontal: 10,
+    width: '80%',
+    margin: 10,
+  },
+  textStyle: {
+    fontSize: 20,
   },
   containerButtons: {
     alignItems: 'center',
@@ -135,15 +126,13 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   botaoVoltar: {
-    paddingLeft: 10
+    paddingLeft: 10,
   },
-  logoff: {
-
-  },
+  logoff: {},
   imageLogoff: {
     width: 40,
     height: 40,
-    marginRight: 10
+    marginRight: 10,
   },
 });
 
