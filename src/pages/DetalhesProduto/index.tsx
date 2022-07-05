ccf0ec19c6b2fe192557b03e6a589c7e6c44681d
Marina Portugal <marinapsvreis@gmail.com>
2022-07-05 18:30:00 -0300

    ajuste formatacao preco pagina detalhes produto

--- src/pages/DetalhesProduto/index.tsx
+++ src/pages/DetalhesProduto/index.tsx
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import {
   View,
   StyleSheet,
@@ -7,19 +7,19 @@ import {
   Image,
   Modal,
 } from 'react-native';
-import {Text} from 'react-native-elements';
+import { Text } from 'react-native-elements';
 import IconMat from 'react-native-vector-icons/MaterialIcons';
 import CardsFavoritos from '../../components/CardProduto';
 import Icon from 'react-native-vector-icons/AntDesign';
 import ButtonVoltarHome from '../../components/buttonVoltarHome';
-import {CarrinhoContext} from '../../context/CarrinhoContext';
+import { CarrinhoContext } from '../../context/CarrinhoContext';
 import Produtos from '../Produtos';
-import {FavoritosContext} from '../../context/FavoritosContext';
+import { FavoritosContext } from '../../context/FavoritosContext';
 import Favoritos from '../Favoritos';
 
-const DetalhesProduto = ({route, navigation}) => {
+const DetalhesProduto = ({ route, navigation }) => {
   const [isPopup, setPopup] = useState(false);
-  const {res, pagOrigem} = route.params;
+  const { res, pagOrigem } = route.params;
   const [favoritoImage, setFavoritoImage] = useState(<></>);
   const [isFavorito, setIsFavorito] = useState(false);
   const [isCarrinho, setIsCarrinho] = useState(false);
@@ -162,7 +162,7 @@ const DetalhesProduto = ({route, navigation}) => {
         <View style={styles.boxConteudo}>
           <View style={styles.boxImagem}>
             <Image
-              style={{width: 175, height: 175}}
+              style={{ width: 175, height: 175 }}
               source={{
                 uri: res.imagemProduto,
               }}
@@ -176,7 +176,7 @@ const DetalhesProduto = ({route, navigation}) => {
                   {res.descricaoProduto}
                 </Text>
               </View>
-              <Text style={styles.precoProduto}>R$ {res.precoProduto}</Text>
+              <Text style={styles.precoProduto}>R$ {parseFloat(res.precoProduto).toFixed(2).replace('.', ',')}</Text>
             </View>
             <View style={styles.boxInfoBaixo}>
               <TouchableOpacity
@@ -195,8 +195,8 @@ const DetalhesProduto = ({route, navigation}) => {
                   loadPopup();
                   isCarrinho
                     ? setMessagePopup(
-                        e => 'Adicionando mais uma unidade ao seu carrinho',
-                      )
+                      e => 'Adicionando mais uma unidade ao seu carrinho',
+                    )
                     : setMessagePopup(e => 'Produto adicionado com sucesso');
                   setIsCarrinho(true);
                 }}>
